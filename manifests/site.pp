0e0282016baf1dd3cf181ee7aa000c7f883a53cc
Robert Waffen <rw@example42.com>
2021-10-25 14:35:11 +0200

    change host names

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,10 +30,10 @@ node default {
   #   class { 'my_class': }
 }
 
-node 'puppetdb.private.example42.cloud' {
+node 'puppetdb.priv.rw.example42.cloud' {
   contain role::puppet::db
 }
 
-node 'puppet.private.example42.cloud' {
+node 'puppet.priv.rw.example42.cloud' {
   contain role::puppet::compiler
 }
